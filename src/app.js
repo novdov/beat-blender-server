b92464a72cf1d9edb2844c148aae57d536025d60
novdov <rpwjd56@gmail.com>
2020-05-16 15:35:45 +0900

    fea) Use port using env variablee

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const modelRouter = require('./routes/model')
 
 const app = express()
 
+const port = process.env.PORT || 8080
+
 app.use(express.json())
 
 app.use('/model', modelRouter)
@@ -12,4 +14,4 @@ app.get('/', (req, res, next) => {
   res.status(200).send('Beat blender server is running\n')
 })
 
-app.listen(8080)
+app.listen(port)
